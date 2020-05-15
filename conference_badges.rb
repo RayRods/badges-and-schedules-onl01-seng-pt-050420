bdc5bc92b064c0eb45c3645c93cff64c0b225340
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-15 06:01:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  attendees.each_with_index
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+   batch_badge_creator(attendees)
+   assign_rooms(attendees)
 end
