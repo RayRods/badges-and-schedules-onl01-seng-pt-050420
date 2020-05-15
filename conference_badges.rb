94c943bcfafd0330d12a10f79bc56cfa09a74c47
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-15 05:47:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 def assign_rooms(attendees)
    attendees.map.each do |name, index|
      index =+ 1
-    puts"Hello, #{name}. You'll be assigned to room #{index}!"
+     "Hello, #{name}. You'll be assigned to room #{index}!"
    end
 end
