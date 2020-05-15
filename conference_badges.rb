a2ac3d017dff7f7c33a86281dd747c788f3e01b4
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-15 05:26:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 
 def batch_badge_creator(attendees)
-  attendees.each do |name| 
-    puts "Hello, my name is #{name}."
+  attendees.each do |names| 
+    puts "Hello, my name is #{names}."
   end
 end
